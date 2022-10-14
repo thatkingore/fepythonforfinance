9243fd3030772473984763a96024c8e81446af45
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-10-15 00:09:55 +0100

    Add files via upload

---
+++ Naming and Storing Values in Variables.py
@@ -0,0 +1,12 @@
+assets = 1000
+print(assets)
+
+liabilities = 5000
+print(liabilities)
+
+assets = assets + 2000
+print(assets)
+
+WACC = "Weighted Average Capital Cost"
+WACC = "Weighted Average Cost of Capital"
+print(WACC)
