a352b2c854158916f6d533d0683af33667e3a7dc
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-10-18 00:13:57 +0100

    Update Naming and Storing Values in Variables.py

--- Naming and Storing Values in Variables.py
+++ Naming and Storing Values in Variables.py
@@ -12,7 +12,8 @@ WACC = "Weighted Average Cost of Capital"
 print(WACC)
 
 current_asset = "Current " + "Assets"
-print(current_asset)
+print(current_asset + " equal 10,000")
+print(current_asset + " equal " + str(10000))
 
 terms = ['NPV', 'DCF', 'Equities', 'FICC', 'LBO', 'M&A']
 print(terms[0])
