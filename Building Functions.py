aaa5aa7a49bb4c26f3ac2653371a2af6edc1afee
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-10-18 13:05:38 +0100

    Update Building Functions.py

--- Building Functions.py
+++ Building Functions.py
@@ -1,3 +1,12 @@
+# IF Statements #
+
+# Equals: x == y
+# Not Equal to: x != y
+# Less than: x < y
+# Greater than: x > y
+# Less than or Equal to: x <= y
+# Greater than or Equal to: x >= y
+
 x = 5
 y = 2
 if x > y:
@@ -6,3 +15,35 @@ elif x == y:
     print("x is equal to y")
 else:
     print("x is greater than y")
+
+# Workout 1 #
+
+x = 2
+y = 3
+
+if y > x:
+    print("y is greater than x")
+
+
+# Workout 2 #
+
+x = 2
+y = 2
+
+if y > x:
+    print("y is greater than x")
+elif y == x:
+    print("y is equal to x")
+
+
+# Workout 3 #
+
+x = 2
+y = 1
+
+if y > x:
+    print("y is greater than x")
+elif y == x:
+    print("y is equal to x")
+else:
+    print("y is less than x")
