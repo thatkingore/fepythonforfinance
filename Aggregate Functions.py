59c7a87d1f051b95b1916d9c466c0e94598e85bc
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-10-19 21:54:24 +0100

    Add files via upload

---
+++ Aggregate Functions.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+SP500_Monday = np.array([105.6, 1206.4, 137.6])
+print(SP500_Monday)
+
+SP500_Tuesday = np.array([(105.6, 1206.4, 137.6), (106.4, 1208.4, 139.9)], dtype = float)
+print(SP500_Tuesday)
