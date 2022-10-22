30c8b7df355b3feb8abc93c4636bbddf9eab9211
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-10-22 18:19:56 +0100

    Update Predicting Future Day.py

--- Predicting Future Day.py
+++ Predicting Future Day.py
@@ -1,5 +1,5 @@
+# Today's Date
 from datetime import date
-from datetime import datetime
 
 today = date.today()
 print("Today is " + str(today))
@@ -10,6 +10,17 @@ print("The year is " + str(today.year))
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 print("Today is " + str(days[today.weekday()]) + "(" + str(today.weekday()) + ")")
 
+# The Time Right Now
+from datetime import datetime
+
 the_time = datetime.now()
 print("The date and time right now is " + str(the_time))
 print("The time right now is " + str(datetime.time(datetime.now())))
+
+# Using Calendars
+
+import calendar
+
+cal = calendar.TextCalendar(calendar.SUNDAY)
+calen = cal.formatmonth(2020, 10, 0, 0)
+print(calen)
