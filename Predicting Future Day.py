ee10bb06b5b7192a30dee90db8c6f14aca237efa
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-10-22 18:36:46 +0100

    Update Predicting Future Day.py

--- Predicting Future Day.py
+++ Predicting Future Day.py
@@ -22,6 +22,18 @@ print("The time right now is " + str(datetime.time(datetime.now())))
 now = datetime.now()
 print("Today's date is " + str(now.strftime("%A")) + ", " + str(now.strftime("%B")) + " " + str(now.strftime("%d")) + " " + str(now.strftime("%Y")))
 
+# Time Formatting
+
+print("12-Hour format time is " + str(now.strftime("%I:%M:%S %p")))
+print("24-Hour format time is " + str(now.strftime("%H:%M:%S")))
+
+
+# Local Data and Time
+
+print("Again, today's date is " + str(now.strftime("%x")))
+print("And the time is " + str(now.strftime("%X")))
+print("The local date and time is " + str(now.strftime("%c")))
+
 # Using Calendars
 
 import calendar
@@ -30,9 +42,3 @@ cal = calendar.TextCalendar(calendar.SUNDAY)
 calen = cal.formatmonth(2020, 10, 0, 0)
 print(calen)
 
-# Local Data and Time
-
-now = datetime.now()
-print("Again, today's date is " + str(now.strftime("%x")))
-print("And the time is " + str(now.strftime("%X")))
-print("The local date and time is " + str(now.strftime("%c")))
